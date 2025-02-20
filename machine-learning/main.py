9c33b0a86eb5b55414b694ea44bee9b3b67a2155
walgneyj <walgney.junior@unifesspa.edu.br>
2025-02-20 18:28:56 -0300

    Removendo arquivos desnecessários

--- machine-learning/main.py
+++ machine-learning/main.py
@@ -1,5 +1,6 @@
 import subprocess
-from visualization import plot_model_results, plot_data_augmentation
+from features.visualization import plot_model_results, plot_data_augmentation
+import pandas as pd
 
 def main():
     # Executar o script features/main.py
@@ -12,8 +13,21 @@ def main():
 
     # Plotar resultados dos modelos e quantidades de aumento de dados
     print("\nPlotando resultados dos modelos e quantidades de aumento de dados...")
-    plot_model_results()
-    plot_data_augmentation()
+    results_df = pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/metrics.xlsx")
+    output_path = "C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results"
+    plot_model_results(results_df, output_path)
+
+    original_counts = {
+        'keyboard': len(pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/original_features.xlsx")),
+        'mouse': len(pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/original_features.xlsx")),
+        'behavior': len(pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/original_features.xlsx"))
+    }
+    augmented_counts = {
+        'keyboard': len(pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/augmented_features.xlsx")),
+        'mouse': len(pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/augmented_features.xlsx")),
+        'behavior': len(pd.read_excel("C:/Users/walgn/OneDrive/Documentos/Trabalho artigo/autenticacao-de-sistemas-baseados-em-biometria-comportamental-main/machine-learning/results/augmented_features.xlsx"))
+    }
+    plot_data_augmentation(original_counts, augmented_counts, output_path)
 
 if __name__ == "__main__":
     main()
